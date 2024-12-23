77730f6629642754461fd7162bd966dce4fc583a
Mahamoud <mahamoudbouralehfarah7@gmail.com>
2024-12-23 15:43:33 +0300

    changement

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,94 @@
-import React from "react";
-import './Contact.css'; // Remplacez "Acceuille" par le nom du composant
-
-
+import React, { useState } from "react";
+import "./Contact.css";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    nom: "",
+    prenom: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted:", formData);
+  };
+
   return (
-    <div>
-      <h2>Contactez-Nous</h2>
-      <p>Pour toute question ou information supplémentaire, vous pouvez nous joindre via les moyens suivants :</p>
-      <ul>
-        <li><strong>Téléphone :</strong> +253 77 15 34 98</li>
-        <li><strong>Fax :</strong> +253 21 36 92 56</li>
-        <li><strong>Email :</strong> <a href="mailto:geosom@gmail.com">geosom@gmail.com</a></li>
-      </ul>
+    <div className="contact-container">
+      <h1>Get in touch</h1>
+      <p>Leave your message and we'll get back to you shortly.</p>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <div className="form-field">
+            <label htmlFor="nom">Nom</label>
+            <input
+              type="text"
+              id="nom"
+              name="nom"
+              placeholder="Nom"
+              value={formData.nom}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="prenom">Prenom</label>
+            <input
+              type="text"
+              id="prenom"
+              name="prenom"
+              placeholder="Prenom"
+              value={formData.prenom}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="form-field">
+            <label htmlFor="email">Email *</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="geosom@gmail.com"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="phone">Phone</label>
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              placeholder="+253 77 15 34 98"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-field">
+          <label htmlFor="message">Message *</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="ecrire votre message ici"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
