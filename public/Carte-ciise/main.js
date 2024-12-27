aa63735d099a9d3cc374bd1268b3b88162e0f9f3
Mahamoud <mahamoudbouralehfarah7@gmail.com>
2024-12-27 14:31:46 +0300

    modification

--- public/Carte-ciise/main.js
+++ public/Carte-ciise/main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([5.902107200912213, 43.30857649710357], 4);
+var map = L.map('map').setView([5.902107200912213, 43.30857649710357], 5);
 
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
