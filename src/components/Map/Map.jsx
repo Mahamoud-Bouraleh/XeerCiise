853300a58a97e470c8cf4d2198cdf09d5efcfe2b
Mahamoud <mahamoudbouralehfarah7@gmail.com>
2024-12-28 11:19:39 +0300

    changement

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,12 +5,13 @@ const Map = () => {
     <div>
       <h2>la Carte de Ciisa</h2>
       <iframe
-        src="/Carte-ciise/carte.html" // Chemin correct vers le fichier HTML dans le dossier public
-        title="Carte du système Xeer Ciise"
-        width="100%"
-        height="500px"
-        style={{ border: "none" }}
-      ></iframe>
+  src="/Carte-ciise/carte.html"
+  title="Carte du système Xeer Ciise"
+  width="100%"
+  height="500px"
+  style={{ border: "none" }}
+></iframe>
+
     </div>
   );
 };
